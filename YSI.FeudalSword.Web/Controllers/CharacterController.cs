dec00a396d3cfeaafb57c5682aa0e9c212c16d86
YagovkinSI <sorpionupgrade@mail.ru>
2022-09-01 07:18:43 +0400

    Create a method for taking control of a character on the backend

--- YSI.FeudalSword.Web/Controllers/CharacterController.cs
+++ YSI.FeudalSword.Web/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
@@ -6,6 +7,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using YSI.FeudalSword.Web.ApiModels;
 using YSI.FeudalSword.Web.Database;
+using YSI.FeudalSword.Web.Database.Models;
+using YSI.FeudalSword.Web.Helpers;
 
 namespace YSI.FeudalSword.Web.Controllers
 {
@@ -14,12 +17,15 @@ namespace YSI.FeudalSword.Web.Controllers
     public class CharacterController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
         private readonly ILogger<UserController> _logger;
 
         public CharacterController(ApplicationDbContext context,
+            UserManager<User> userManager,
             ILogger<UserController> logger)
         {
             _context = context;
+            _userManager = userManager;
             _logger = logger;
         }
 
@@ -52,6 +58,44 @@ namespace YSI.FeudalSword.Web.Controllers
                 _logger.LogError(ex.Message, ex);
                 return BadRequest(ex.Message);
             }
+        }
+
+        [HttpPost]
+        [Route("takeСontrol")]
+        public async Task<ActionResult<bool>> TakeСontrol(int characterId)
+        {
+            try
+            {
+                var currentUser = await _userManager.GetUserAsync(_context, HttpContext.User);
+                if (currentUser == null)
+                    return NotFound("Не удалось определить текущего пользователя.");
+
+                var character = _context.Characters
+                    .Include(c => c.Dynasty)
+                    .FirstOrDefault(c => c.UserId == currentUser.Id);
+                if (character != null)
+                    return NotFound($"Текущим пользователем {currentUser.UserName} " +
+                        $"уже взят персонаж {character.Name} {character.Dynasty.Name}.");
+
+                character = _context.Characters
+                    .Include(c => c.Dynasty)
+                    .Include(c => c.User)
+                    .Single(c => c.Id == characterId);
+
+                if (character.UserId != null)
+                    return NotFound($"Персонаж {character.Name} {character.Dynasty.Name} " +
+                        $"уже взят игроком {character.User.UserName}.");
+
+                character.UserId = currentUser.Id;
+                await _context.SaveChangesAsync();
+                _logger.LogInformation($"User {currentUser.UserName} took character {character.Name} {character.Dynasty.Name} (id - {character.Id})");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message.ToString());
+            }            
         }
     }
 }
